3c665250ff958df901e2867493ee0d438840a95d
Hellesandro Gonzaga <hellesandro@hotmail.com>
2018-10-04 08:24:51 -0300

    PMC
    Organizacao PMC

--- Projeto Java/S-Organizer/S-Organizer/src/modelo/Categoria.java
+++ Projeto Java/S-Organizer/S-Organizer/src/modelo/Categoria.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package s.organizer;
+package modelo;
 
 /**
  *
--- Projeto Java/S-Organizer/S-Organizer/src/modelo/ProtocoloGenerator.java
+++ Projeto Java/S-Organizer/S-Organizer/src/modelo/ProtocoloGenerator.java
@@ -1,4 +1,4 @@
-package s.organizer;
+package modelo;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
