a337f9d67ec9e41b7fd2d6d75c8b3176549a3b95
Hellesandro Gonzaga <hellesandro@hotmail.com>
2018-11-08 00:18:14 -0200

    #ficouparatras
    att

--- Projeto Java/S-Organizer/S-Organizer/src/modelo/PadroesDeProjeto/Builder/DiretorAgenda.java
+++ Projeto Java/S-Organizer/S-Organizer/src/modelo/PadroesDeProjeto/Builder/DiretorAgenda.java
@@ -22,6 +22,6 @@ public class DiretorAgenda {
                 
         BuilderAgenda builder = new BuilderAgenda(horaInicioExpediente, horaFimExpediente, servicosDoDia, prestador, agenda );
         
-        return 
+        return agenda;
     }
 }
