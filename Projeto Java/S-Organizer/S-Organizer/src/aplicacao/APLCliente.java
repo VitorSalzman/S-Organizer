01ff9498d8d4590f666389dd71bc3e4df5e3e220
Luiz Antonio R. G <luizguzzo@gmail.com>
2018-12-01 19:15:31 -0200

    pegar solicitação pelo protocolo "implementada"
    ja existia uma função q fazia isso

--- Projeto Java/S-Organizer/S-Organizer/src/aplicacao/APLCliente.java
+++ Projeto Java/S-Organizer/S-Organizer/src/aplicacao/APLCliente.java
@@ -5,13 +5,26 @@
  */
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.ControladoresJPA.SolicitacaoJpaController;
+import modelo.Solicitacao;
+
 /**
  *
  * @author vitorsalzman
  */
 public class APLCliente {
-    public void consultaSolicitacao(double protocolo){
+    public Solicitacao consultaSolicitacao(long protocolo){
         //public Solicitacao consultaSolicitacao(double protocolo){   ///Na verdade é esse
+        
+        EntityManagerFactory objFactory = Persistence.createEntityManagerFactory("S-Organizer2PU");
+        EntityManager manager = objFactory.createEntityManager();
+        
+        SolicitacaoJpaController solicitacaoJpaController = new SolicitacaoJpaController(objFactory);
+        
+        return solicitacaoJpaController.findSolicitacao(protocolo);
         
         //Aqui é uma busca no bd por uma solicitacao com um protocolo igual ao passado por parametro; 
     }
