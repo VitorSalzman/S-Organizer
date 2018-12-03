4286ac77d0dbefe2467ce3eaac98b30f8649353c
VitorSalzman <31069168+VitorSalzman@users.noreply.github.com>
2018-12-03 09:34:37 -0200

    Alteração de Modelo Físico
    Alterações devido novo dump

--- Arquivos SQL/Modelo Físico.sql
+++ Arquivos SQL/Modelo Físico.sql
@@ -4,16 +4,16 @@ CREATE TABLE agenda
     data date,
     horafim time without time zone,
     horainicio time without time zone,
-    empresa_idusuario bigint,
-    prestador_idusuario bigint,
+    empresa_id_usuario bigint,
+    prestador_id_usuario bigint,
 	
     CONSTRAINT agenda_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_agenda_empresa_idusuario FOREIGN KEY (empresa_idusuario)
-        REFERENCES empresa (idusuario) MATCH SIMPLE,   
-    CONSTRAINT fk_agenda_prestador_idusuario FOREIGN KEY (prestador_idusuario)
-        REFERENCES prestador (idusuario) MATCH SIMPLE,
+    CONSTRAINT fk_agenda_empresa_id_usuario FOREIGN KEY (empresa_id_usuario)
+        REFERENCES empresa (id_usuario) MATCH SIMPLE,   
+    CONSTRAINT fk_agenda_prestador_id_usuario FOREIGN KEY (prestador_id_usuario)
+        REFERENCES prestador (id_usuario) MATCH SIMPLE
         
-)
+);
 
 CREATE TABLE agenda_solicitacao
 (
@@ -26,7 +26,7 @@ CREATE TABLE agenda_solicitacao
        
     CONSTRAINT fk_agenda_solicitacao_solicitacaododia_protocolo FOREIGN KEY (solicitacaododia_protocolo)
         REFERENCES solicitacao (protocolo) MATCH SIMPLE
-)
+);
 
 CREATE TABLE atendimento
 (
@@ -35,13 +35,13 @@ CREATE TABLE atendimento
     dataatendimento date,
     horariofim time without time zone,
     horarioinicio time without time zone,
-    prestador_idusuario bigint,
+    prestador_id_usuario bigint,
 	
     CONSTRAINT atendimento_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_atendimento_prestador_idusuario FOREIGN KEY (prestador_idusuario)
-        REFERENCES prestador (idusuario) MATCH SIMPLE
+    CONSTRAINT fk_atendimento_prestador_id_usuario FOREIGN KEY (prestador_id_usuario)
+        REFERENCES prestador (id_usuario) MATCH SIMPLE
         
-)
+);
 
 CREATE TABLE bairro
 (
@@ -52,7 +52,7 @@ CREATE TABLE bairro
     CONSTRAINT bairro_pkey PRIMARY KEY (id),
     CONSTRAINT fk_bairro_cidade_id FOREIGN KEY (cidade_id)
         REFERENCES cidade (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE categoriaservico
 (
@@ -61,7 +61,7 @@ CREATE TABLE categoriaservico
     duracao time without time zone,
 	
     CONSTRAINT categoriaservico_pkey PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE cidade
 (
@@ -72,52 +72,52 @@ CREATE TABLE cidade
     CONSTRAINT cidade_pkey PRIMARY KEY (id),
     CONSTRAINT fk_cidade_estado_id FOREIGN KEY (estado_id)
         REFERENCES estado (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE cliente
 (
-    idusuario bigint NOT NULL,
+    id_usuario bigint NOT NULL,
     cpf character varying(20),
     nome character varying(50),
     telefone character varying(20),
-    CONSTRAINT cliente_pkey PRIMARY KEY (idusuario)
-)
+    CONSTRAINT cliente_pkey PRIMARY KEY (id_usuario)
+);
 
 CREATE TABLE cliente_solicitacao
 (
-    cliente_idusuario bigint NOT NULL,
+    cliente_id_usuario bigint NOT NULL,
     solicitacao_protocolo bigint NOT NULL,
 	
-    CONSTRAINT cliente_solicitacao_pkey PRIMARY KEY (cliente_idusuario, solicitacao_protocolo),
-    CONSTRAINT fk_cliente_solicitacao_cliente_idusuario FOREIGN KEY (cliente_idusuario)
-        REFERENCES cliente (idusuario) MATCH SIMPLE,
+    CONSTRAINT cliente_solicitacao_pkey PRIMARY KEY (cliente_id_usuario, solicitacao_protocolo),
+    CONSTRAINT fk_cliente_solicitacao_cliente_id_usuario FOREIGN KEY (cliente_id_usuario)
+        REFERENCES cliente (id_usuario) MATCH SIMPLE,
     CONSTRAINT fk_cliente_solicitacao_solicitacao_protocolo FOREIGN KEY (solicitacao_protocolo)
         REFERENCES solicitacao (protocolo) MATCH SIMPLE    
-)
+);
 
 CREATE TABLE empresa
 (
-    idusuario bigint NOT NULL,
+    id_usuario bigint NOT NULL,
     cnpj character varying(20),
     login character varying(255),
     nome character varying(50),
     senha character varying(255),
     telefone character varying(20),
 	
-    CONSTRAINT empresa_pkey PRIMARY KEY (idusuario)
-)
+    CONSTRAINT empresa_pkey PRIMARY KEY (id_usuario)
+);
 
 CREATE TABLE empresa_prestador
 (
-    empresa_idusuario bigint NOT NULL,
-    prestadores_idusuario bigint NOT NULL,
+    empresa_id_usuario bigint NOT NULL,
+    prestadores_id_usuario bigint NOT NULL,
 	
-    CONSTRAINT empresa_prestador_pkey PRIMARY KEY (empresa_idusuario, prestadores_idusuario),
-    CONSTRAINT fk_empresa_prestador_empresa_idusuario FOREIGN KEY (empresa_idusuario)
-        REFERENCES empresa (idusuario) MATCH SIMPLE,
-    CONSTRAINT fk_empresa_prestador_prestadores_idusuario FOREIGN KEY (prestadores_idusuario)
-        REFERENCES prestador (idusuario) MATCH SIMPLE
-)
+    CONSTRAINT empresa_prestador_pkey PRIMARY KEY (empresa_id_usuario, prestadores_id_usuario),
+    CONSTRAINT fk_empresa_prestador_empresa_id_usuario FOREIGN KEY (empresa_id_usuario)
+        REFERENCES empresa (id_usuario) MATCH SIMPLE,
+    CONSTRAINT fk_empresa_prestador_prestadores_id_usuario FOREIGN KEY (prestadores_id_usuario)
+        REFERENCES prestador (id_usuario) MATCH SIMPLE
+);
 
 CREATE TABLE empresa_solicitacao
 (
@@ -129,7 +129,7 @@ CREATE TABLE empresa_solicitacao
         REFERENCES empresa (id_usuario) MATCH SIMPLE,
     CONSTRAINT fk_empresa_solicitacao_solicitacaoes_protocolo FOREIGN KEY (solicitacaoes_protocolo)
         REFERENCES solicitacao (protocolo) MATCH SIMPLE,
-)
+);
 
 CREATE TABLE endereco
 (
@@ -141,7 +141,7 @@ CREATE TABLE endereco
     CONSTRAINT endereco_pkey PRIMARY KEY (id),
     CONSTRAINT fk_endereco_logradouro_id FOREIGN KEY (logradouro_id)
         REFERENCES logradouro (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE estado
 (
@@ -149,7 +149,7 @@ CREATE TABLE estado
     estado character varying(15),
 	
     CONSTRAINT estado_pkey PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE logradouro
 (
@@ -160,11 +160,11 @@ CREATE TABLE logradouro
     CONSTRAINT logradouro_pkey PRIMARY KEY (id),
     CONSTRAINT fk_logradouro_bairro_id FOREIGN KEY (bairro_id)
         REFERENCES bairro (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE prestador
 (
-    idusuario bigint NOT NULL,
+    id_usuario bigint NOT NULL,
     cargahoraria integer,
     cpf character varying(20),
     disponibilidade boolean,
@@ -175,9 +175,9 @@ CREATE TABLE prestador
     senha character varying(255),
     telefone character varying(20),
 	
-    CONSTRAINT prestador_pkey PRIMARY KEY (idusuario),
+    CONSTRAINT prestador_pkey PRIMARY KEY (id_usuario)
     
-)
+);
 
 
 CREATE TABLE prestador_agenda
@@ -188,8 +188,8 @@ CREATE TABLE prestador_agenda
     CONSTRAINT fk_prestador_agenda_agenda_id FOREIGN KEY (agenda_id)
         REFERENCES agenda (id) MATCH SIMPLE,
     CONSTRAINT fk_prestador_agenda_prestador_id_usuario FOREIGN KEY (prestador_id_usuario)
-        REFERENCES prestador (id_usuario) MATCH SIMPLE,
-)
+        REFERENCES prestador (id_usuario) MATCH SIMPLE
+);
 
 CREATE TABLE servico
 (
@@ -205,7 +205,7 @@ CREATE TABLE servico
     CONSTRAINT fk_servico_categoria_id FOREIGN KEY (categoria_id)
         REFERENCES categoriaservico (id) MATCH SIMPLE
         
-)
+);
 
 CREATE TABLE servico_atendimento
 (
@@ -217,7 +217,7 @@ CREATE TABLE servico_atendimento
         REFERENCES atendimento (id) MATCH SIMPLE,
     CONSTRAINT fk_servico_atendimento_servico_id FOREIGN KEY (servico_id)
         REFERENCES servico (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE solicitacao
 (
@@ -233,7 +233,7 @@ CREATE TABLE solicitacao
     CONSTRAINT solicitacao_pkey PRIMARY KEY (protocolo),
     CONSTRAINT fk_solicitacao_endereco_id FOREIGN KEY (endereco_id)
         REFERENCES endereco (id) MATCH SIMPLE
-)
+);
 
 CREATE TABLE solicitacao_servico
 (
@@ -245,4 +245,4 @@ CREATE TABLE solicitacao_servico
         REFERENCES servico (id) MATCH SIMPLE,
     CONSTRAINT fk_solicitacao_servico_solicitacao_protocolo FOREIGN KEY (solicitacao_protocolo)
         REFERENCES solicitacao (protocolo) MATCH SIMPLE
-)
+);
