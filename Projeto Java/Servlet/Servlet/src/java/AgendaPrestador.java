8d21de56e398ffa325748b77b0b9c7cb3ad06a44
Serenna <serenna.ferrari@hotmail.com>
2018-12-03 13:41:23 -0200

    No words

--- Projeto Java/Servlet/Servlet/src/java/AgendaPrestador.java
+++ Projeto Java/Servlet/Servlet/src/java/AgendaPrestador.java
@@ -5,9 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelo.padroesdeprojeto.builder.BuilderSolicitacao;
-import modelo.padroesdeprojeto.builder.DiretorServico;
-import modelo.padroesdeprojeto.builder.DiretorSolicitacao;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,56 +16,70 @@ import modelo.padroesdeprojeto.builder.DiretorSolicitacao;
  *
  * @author Serenna
  */
-public class RecebeDadosCadSolic extends HttpServlet{
+public class AgendaPrestador extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            
-            DiretorSolicitacao dirSolic = new DiretorSolicitacao();
-            DiretorServico dirServico = new DiretorServico();
-            
-            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet RecebeDados</title>");            
+            out.println("<title>Servlet ControleApi</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servidor Recebeu:<br></h1>"); 
-            out.println("<h1>Modelo do carro: " + request.getParameterMap().get("descricao") + "</h1>");
-            out.println("<h1>Placa: " + request.getParameter("placa") + "</h1>");
-            out.println("<h1>Renavam: " + request.getParameter("renavam") + "</h1>");
-            out.println("<h1>Ano: " + request.getParameter("ano") + "</h1>");
-            out.println("<h1>Kilometragem: " + request.getParameter("kms") + "</h1>");
-            out.println("<h1>Acessorios: " + request.getParameter("acessorios") + "</h1>");
-            out.println("<h1>Valor: " + request.getParameter("valor") + "</h1>");
+            out.println("<h1>Servlet ControleApi at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-        } finally {
-            out.close();
         }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
- 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
- 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
-        return "Servlet Recebe Dados";
+        return "Short description";
     }// </editor-fold>
-
 }
